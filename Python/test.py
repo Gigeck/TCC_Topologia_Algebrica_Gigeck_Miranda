7ab299cd6758548b4cd4dfe15db09bb37d219fa2
Gigeck <guilherme.gigeck@gmail.com>
2025-08-16 10:33:48 -0300

    fixed cliques, added independent

--- Python/test.py
+++ Python/test.py
@@ -13,36 +13,30 @@ def neighborComplexSimp(graph):
 
 def cliqueComplexSimp(graph):
     simplex_tree_ret = gd.SimplexTree()
-    cliques = nx.find_cliques(graph)
+    cliques = nx.enumerate_all_cliques(graph)
+    #adds a lot of repetitive cliques, but it is fine
     for i in cliques:
         simplex_tree_ret.insert(i)
-    #toAdd = []
-    #for g in graph.nodes:
-    #    toAdd = []
-    #    toAdd.insert(g)
-        #initial piece of clique containing g
-    #    for x in graph.adj[g]:
-    #        toAdd.append(x)
-    #        for y in graph.adj[g]:
-    #            if (y in toAdd or y < x): continue
-    #            flag = 1
-    #            for k in toAdd:
-    #                if y not in graph.adj[k]: flag=0
-    #            if flag==1:
-    #                toAdd.insert(y)
-    #        #clique added
-    #        simplex_tree_ret.append(toAdd)
-    #        toAdd=[]
-    #        toAdd.insert(g)
     return simplex_tree_ret
+
+def independentComplexSimp(graph):
+    cG = nx.complement(graph)
+    simplex_tree_ret = cliqueComplexSimp(cG)
+    return simplex_tree_ret
+
+def acyclicComplexSimp(graph):
+    #no idea how to do this one
+    pass
+
 
 def main():
     G = nx.path_graph(10)
-    simpTree = cliqueComplexSimp(G)
-    for sk_value in simpTree.get_skeleton(2):
+    simpTree = independentComplexSimp(G)
+    for sk_value in simpTree.get_skeleton(4):
         print(sk_value)
     print("I work")
 
 if __name__=="__main__":
     main()
 
+#NOTES TO SELF: check GraphInducedComplex, maybe useful
